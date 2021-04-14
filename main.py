cc0d4c898b7272a47babd12f738e3b24283a4608
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-04-14 20:39:49 +0200

    added lecture methods

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class HelloResp(BaseModel):
+    msg: str
+
 
 app = FastAPI()
+app.counter = 0
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return app.counter
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+
+@app.get("/hello/{name}", response_model=HelloResp)
+def hello_name_view(name: str):
+    return HelloResp(msg=f"Hello {name}")
