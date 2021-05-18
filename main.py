da211ca6104bfaf6c06841a96c3c6e09c3ff817c
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-05-19 00:43:20 +0200

    updated 5.1

--- main.py
+++ main.py
@@ -529,3 +529,16 @@ def delete_category(id: int):
             raise HTTPException(status_code=404)
         connection.commit()
         return {"deleted": 1}
+
+
+@app.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
+async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
+    db_shipper = crud.get_shipper(db, shipper_id)
+    if db_shipper is None:
+        raise HTTPException(status_code=404, detail="Shipper not found")
+    return db_shipper
+
+
+@app.get("/shippers", response_model=List[schemas.Shipper])
+async def get_shippers(db: Session = Depends(get_db)):
+    return crud.get_shippers(db)
