5eff86554ab123bf927493d14d0491b8adbef5ff
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-05-18 20:54:29 +0200

    updated 5.1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import uuid
 from contextlib import contextmanager
 from datetime import datetime
 from hashlib import sha512
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy.orm import Session
 
@@ -528,10 +528,14 @@ def delete_category(id: int):
         return {"deleted": 1}
 
 
-@app.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
-async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
-    db_shipper = crud.get_shipper(db, shipper_id)
-    if db_shipper is None:
+@app.get("/suppliers/{supplier_id}", response_model=schemas.Shipper)
+async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
+    db_supplier = crud.get_supplier(db, supplier_id)
+    if db_supplier is None:
         raise HTTPException(status_code=404, detail="Shipper not found")
-    return db_shipper
+    return db_supplier
 
+
+@app.get("/suppliers", response_model=List[schemas.Supplier])
+async def get_suppliers(db: Session = Depends(get_db)):
+    return crud.get_suppliers(db)
--- schemas.py
+++ schemas.py
@@ -8,3 +8,21 @@ class Shipper(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Supplier(BaseModel):
+    SupplierID = PositiveInt
+    CompanyName = constr(max_length=40)
+    ContactName = constr(max_length=30)
+    ContactTitle = constr(max_length=30)
+    Address = constr(max_length=60)
+    City = constr(max_length=15)
+    Region = constr(max_length=15)
+    PostalCode = constr(max_length=10)
+    Country = constr(max_length=15)
+    Phone = constr(max_length=24)
+    Fax = constr(max_length=24)
+    HomePage = str
+
+    class Config:
+        orm_mode = True
