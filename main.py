d01f73e76527b60c14d67a266ad8e8b9e64a15b8
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2021-05-18 22:01:09 +0200

    updated 5.1

--- main.py
+++ main.py
@@ -549,7 +549,7 @@ async def get_supplier(supplier_id: PositiveInt):
         port=port
     )
     cursor = ps_conn.cursor()
-    db_supplier = cursor.execute('SELECT * FROM Suppliers WHERE SupplierId=?', (supplier_id,)).fetchone()
+    db_supplier = cursor.execute('SELECT * FROM Suppliers WHERE suppliers.SupplierID=?', (supplier_id,)).fetchone()
     if db_supplier is None:
         raise HTTPException(status_code=404, detail="Supplier not found")
     parsed = {
